37f4505992000ad144d4f57e0e4dcabbeab23f79
mc26002-hash <mc26002@esfe.agape.edu.sv>
2026-04-06 18:12:30 -0600

    Agregar archivos de proyecto.

---
+++ JAMC20260406.AppSeguridadWeb/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using JAMC20260406.AppSeguridadWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace JAMC20260406.AppSeguridadWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
