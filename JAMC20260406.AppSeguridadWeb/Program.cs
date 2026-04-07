c62b325ab91a66193c4611dc829b0b0aa7cbc3fa
mc26002-hash <mc26002@esfe.agape.edu.sv>
2026-04-06 18:22:10 -0600

    Angel: Modificacion del archivo Program.cs

--- JAMC20260406.AppSeguridadWeb/Program.cs
+++ JAMC20260406.AppSeguridadWeb/Program.cs
@@ -1,10 +1,16 @@
+using JAMC20260406.AppSeguridadWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+// Configurar la conexión a SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
